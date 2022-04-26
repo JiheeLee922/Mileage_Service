28b64066957c1962b7e257cab68e8fa8820c9b14
JiheeLee922 <dhkdnun@naver.com>
2022-04-26 22:24:01 +0900

    [add] 프로젝트 생성

---
+++ triple_mileage/src/main/java/com/triple/mileage/TripleMileageApplication.java
@@ -0,0 +1,13 @@
+package com.triple.mileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleMileageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleMileageApplication.class, args);
+	}
+
+}
---
+++ triple_mileage/src/test/java/com/triple/mileage/TripleMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.mileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleMileageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
